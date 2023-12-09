042cc34581c5a352ca721862d6e2847cb7b9ca18
NguyenCanhPhong <Nguyencanhphong246@gmail.com>
2023-12-09 18:40:21 +0700

    Add project files.

---
+++ BookShop/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShop.Models
+{
+    [Table("Order")]
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string  UserId { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public int OrderStatusId { get; set; }
+        public int OrderStatusId { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+
+    }
+}
